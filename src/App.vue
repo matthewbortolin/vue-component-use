1fc902689884896279b6874145e38977dd3a9f65
Matt <matthewbortolin@hotmail.com>
2018-04-24 23:09:44 +1000

    recieve new quote from component, push to array

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid> 
   </div>
 </template>
 
 <script>
   import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+
   export default {
     data () {
       return {
@@ -13,8 +16,14 @@
         maxQuotes: 10
       }
     },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote); 
+      }
+    },
     components: {
-      'app-quote-grid': QuoteGrid 
+      'app-quote-grid': QuoteGrid,
+      'app-new-quote': NewQuote 
     }
   }
 </script>
